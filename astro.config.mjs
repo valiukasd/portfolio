c1310b95c9f02ab1e9682db43d3859e5ae1dda01
valiukasd <deivis.valiukas@gmail.com>
2023-06-30 13:40:16 +0300

    Add react and vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  output: "static",
+  adapter: vercel({
+    analytics: true,
+    imagesConfig: {
+      sizes: [],
+      domains: [],
+    },
+  }),
+});
